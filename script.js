5bb1f81487df2a8dd685408408fb8e0de9b40bd9
OrlandoJC <cuximyamaorlandodejesus@gmail.com>
2020-12-15 02:53:44 -0600

    Mejoras v2

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 (function() {
 
     var intentos = 0;
+    var adivinados = 0;
+    var audioCard = new Audio("music/page-flip-18.wav");
+    var check = new Audio("music/points.mp3");
+    var gameOver = new Audio("music/Game-over-ident.mp3")
+    var winAudio = new Audio("music/win.mp3")
+    var loopAudio = new Audio("music/musicloop.mp3")
+    var fail = new Audio("music/Fail-sound.mp3")
+    var timerAudio = new Audio("music/Timer-clicking-sound.mp3")
+    var temp = false;
+
+    var seg = 0;
+
+
+
+    gameOver.volume = 0.8;
+    winAudio.volume = 0.8;
+    loopAudio.volume = 0.8;
+
+
     var Memory = {
 
         init: function(cards) {
@@ -9,8 +28,13 @@
             this.$message = $(".winner");
             this.$overlay = $(".modal-overlay");
             this.$restartButton = $("button.restart");
+            this.$points = $("#intentos");
+            this.$correctos = $("#correctos");
+            this.$reloj = $("#reloj");
             this.cardsArray = $.merge(cards, cards);
+            this.timerSet;
             this.shuffleCards(this.cardsArray);
+
             this.setup();
         },
 
@@ -24,40 +48,72 @@
             this.$memoryCards = $(".card");
             this.paused = false;
             this.guess = null;
+            this.$points.text("0");
+
             this.binding();
         },
 
         binding: function() {
             this.$memoryCards.on("click", this.cardClicked);
             this.$restartButton.on("click", $.proxy(this.reset, this));
+            this.$reloj.on("click", this.temp)
         },
 
         cardClicked: function() {
             var _ = Memory;
+
             var $card = $(this);
 
-            intentos += 1;
+
+            _.$points.text(intentos);
+            audioCard.cloneNode(true).play()
+
+
             if (!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")) {
                 $card.find(".inside").addClass("picked");
                 if (!_.guess) {
                     _.guess = $(this).attr("data-id");
+
+                    intentos += 1;
                 } else if (_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")) {
                     $(".picked").addClass("matched");
                     _.guess = null;
+                    adivinados += 1
+
+                    _.$correctos.text(adivinados);
+                    check.cloneNode(true).play()
+
+
+
                 } else {
                     _.guess = null;
                     _.paused = true;
+
                     setTimeout(function() {
+                        fail.play();
                         $(".picked").removeClass("picked");
                         Memory.paused = false;
                     }, 600);
+
+                    setTimeout(function() {
+
+                        fail.pause();
+                        fail.currentTime = 0;
+
+                    }, 1800);
+
+
+
                 }
 
-                if (intentos == 48) {
+
+                if (intentos == 29 && temp == false) {
                     _.win(false);
 
                     return;
                 }
+
+
                 if ($(".matched").length == $(".card").length) {
                     _.win(true);
                 }
@@ -69,7 +125,12 @@
             this.paused = true;
             setTimeout(function() {
 
-                if (!status) that.$message.text("Perdiste! :(");
+                if (!status) {
+                    that.$message.text("Perdiste! :(");
+                    gameOver.play();
+                } else {
+                    winAudio.play();
+                }
                 Memory.showModal();
                 Memory.$game.fadeOut();
             }, 1000);
@@ -90,19 +151,29 @@
             this.shuffleCards(this.cardsArray);
             this.setup();
             this.$game.show("slow");
+            this.$reloj.text("Temporizador")
+            this.$correctos.text("0")
             intentos = 0;
+            seg = 0;
+            adivinados = 0;
+
+
+            clearInterval(this.timerSet);
+            temp = false;
+
+
+
+
+
+
         },
 
         shuffle: function(array) {
             var counter = array.length,
                 temp, index;
-            // While there are elements in the array
             while (counter > 0) {
-                // Pick a random index
                 index = Math.floor(Math.random() * counter);
-                // Decrease counter by 1
                 counter--;
-                // And swap the last element with it
                 temp = array[counter];
                 array[counter] = array[index];
                 array[index] = temp;
@@ -116,12 +187,40 @@
                 frag += '<div class="card" data-id="' + v.id + '"><div class="inside">\
 				<div class="front"><img src="' + v.img + '"\
 				alt="' + v.name + '" /></div>\
-				<div class="back"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThUqsyUOGRdJSErbfxvtASout-NNmGZtpAcA&usqp=CAU"\
+				<div class="back"><img src="imgs/question-mark.png"\
 				alt="Codepen" /></div></div>\
 				</div>';
             });
             return frag;
+        },
+        temp: function() {
+            var _ = Memory;
+            if (temp == false && intentos == 0 && adivinados == 0) {
+                temp = true;
+                timerAudio.play();
+                timerAudio.loop = true;
+                _.timerSet = setInterval(_.timer, 1000);
+            }
+        },
+        timer: function() {
+            var _ = Memory;
+            seg += 1;
+            var minutes = Math.floor(seg / 60),
+                seconds = Math.floor(seg - minutes * 60),
+                x = minutes < 10 ? "0" + minutes : minutes,
+                y = seconds < 10 ? "0" + seconds : seconds;
+
+            if (seg == 114) {
+                timerAudio.currentTime = 0;
+                timerAudio.pause();
+
+                _.win(false);
+
+                return;
+            }
+            console.log(Memory.$reloj.text(x + " : " + y))
         }
+
     };
 
     var cards = [{
@@ -186,9 +285,6 @@
         },
     ];
 
-
-
     Memory.init(cards);
-
 
 })();
